09e089a777ec44d33800dde4681e4beb5c4392ea
unknown <dom1nik@mail.ru>
2020-10-11 13:19:40 +0300

    add checking for negative numbets

--- jd1-module1-basic/src/by/htp/homework/basic/BranchingTask1.java
+++ jd1-module1-basic/src/by/htp/homework/basic/BranchingTask1.java
@@ -15,7 +15,7 @@ public class BranchingTask1 {
 
 		if ((angleOne + angleTwo) == 90) {
 			System.out.println(" This triangle exists and it has an angle equal 90");
-		} else if ((angleOne + angleTwo) < 180) {
+		} else if ((angleOne + angleTwo < 180) && (angleOne + angleTwo > 0)) {
 			System.out.println("This triangle exists");
 		} else {
 			System.out.println("Such triangle doesn't exist");
